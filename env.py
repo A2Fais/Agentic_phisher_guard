df03c96520abc313c207453e79eb616151b62d18
A2Fyise <aseeralfaisal@gmail.com>
2025-04-11 23:04:21 +0600

    rename

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ import numpy as np
 class Environment(gym.Env):
     def __init__(self, data):
         super(Environment, self).__init__()
-        filtered_integer_columns = data.select_dtypes(
+        filtered_int_columns = data.select_dtypes(
             include=[np.integer]).columns
         
-        print(len(filtered_integer_columns), filtered_integer_columns)
+        print(len(filtered_int_columns), filtered_int_columns)
